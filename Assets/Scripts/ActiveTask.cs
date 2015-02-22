bee999cd68510f8f02da09d0716c0c2c8b00675b
unknown <go4113@GDD-38.main.ad.rit.edu>
2015-02-22 15:42:26 -0500

    Added 2 Input functions for ActiveTask.

--- Assets/Scripts/ActiveTask.cs
+++ Assets/Scripts/ActiveTask.cs
@@ -1,8 +1,33 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-public class ActiveTask : Task {
+public class ActiveTask : Task 
+{
+	public System.Action<ActiveTask> inputAction;
+	public float previousInputAngle;
+	public Stack<string> InputIdentifiers { get; set; };
 
-	
-	public System.Action InputBehaviour { get; set; }
+	public void ListenJoyStickRotation(ActiveTask task)
+	{
+		var xAxis = Input.GetAxis ("Horizontal");
+		var yAxis = Input.GetAxis ("Vertical");
+		var vector = Vector2 (xAxis, yAxis);
+		previousInputAngle += Mathf.Atan2 (yAxis, xAxis) * Mathf.Rad2Deg;
+		if (previousInputAngle >= 360.0f) {
+			// Signal Done
+		}
+	}
+
+	public void ListenButtonPress(ActiveTask task) 
+	{
+
+		if (Input.GetButtonUp (InputIdentifiers.Peek)) {
+			if (InputIdentifiers.Count == 0) {
+				// Signal Done
+			}
+			else {
+				InputIdentifiers.Pop();
+			}	
+		}
+	}
 }
