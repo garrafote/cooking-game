2a907326a08613ad3fb5e15623bfdd7bbaf5d96d
Henrique Chaltein <hxs1151@rit.edu>
2015-02-22 21:32:51 -0500

    Game Menu Scene

---
+++ Assets/Scripts/GameMenuScrpt.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMenuScrpt : MonoBehaviour {
+
+	public void LoadLevel(string LevelName)
+    {
+        Application.LoadLevel(LevelName);
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+
+    }
+}
